829c2c4d51303b0255094ee7e5b6c550cd1cb345
Francisco Rojas <josefcorojas@gmail.com>
2014-01-12 21:33:22 -0600

    updated the db seed so that there is at least one article per store

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ Article.create(name: 'Super Tennis', description: 'Nice confortable tennis', pri
 Article.create(name: 'Super Boots', description: 'Big shinny boots', price: 175.43,
                total_in_shelf: 2, total_in_vault: 3, store_id: 1)
 Article.create(name: 'Super Sandals', description: 'Water proof sandals', price: 127.43,
-               total_in_shelf: 5, total_in_vault: 1, store_id: 1)
+               total_in_shelf: 5, total_in_vault: 1, store_id: 2)
 Article.create(name: 'Super Shoes', description: 'Nice elegant shoes', price: 347.43,
-               total_in_shelf: 2, total_in_vault: 3, store_id: 1)
+               total_in_shelf: 2, total_in_vault: 3, store_id: 3)
 Article.create(name: 'Super hiking boots', description: 'Big strong boots', price: 111.43,
                total_in_shelf: 1, total_in_vault: 5, store_id: 3)
 Article.create(name: 'Super bowling shoes', description: 'Best antiskid shoes', price: 227.43,
-               total_in_shelf: 1, total_in_vault: 5, store_id: 3)
+               total_in_shelf: 1, total_in_vault: 5, store_id: 4)
 
