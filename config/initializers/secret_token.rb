b4d9521575d87c94fe299590fbb53a3a7ef384cd
Francisco Rojas <josefcorojas@gmail.com>
2014-01-12 21:27:09 -0600

    added the missing secrete_token.rb file, and some seeds for the db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuperZapatos::Application.config.secret_key_base = 'd0beae43c5df1f3ac4cbac158ccff5a53e4e8bbc81113dab5b05130022bd5b9ee4034ed1670b73869d23d28feede272fe187e238d5d5afae00e3e3b5a0aecd01'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Store.create(name: 'Super Zapatos Limon', address: 'Somewhere en Limon')
+Store.create(name: 'Super Zapatos Heredia', address: 'Somewhere en Heredia')
+Store.create(name: 'Super Zapatos Guanacaste', address: 'Somewhere en Guanacaste')
+Store.create(name: 'Super Zapatos Puntarenas', address: 'Somewhere en Puntarenas')
+
+Article.create(name: 'Super Tennis', description: 'Nice confortable tennis', price: 127.43,
+               total_in_shelf: 1, total_in_vault: 2, store_id: 1)
+Article.create(name: 'Super Boots', description: 'Big shinny boots', price: 175.43,
+               total_in_shelf: 2, total_in_vault: 3, store_id: 1)
+Article.create(name: 'Super Sandals', description: 'Water proof sandals', price: 127.43,
+               total_in_shelf: 5, total_in_vault: 1, store_id: 1)
+Article.create(name: 'Super Shoes', description: 'Nice elegant shoes', price: 347.43,
+               total_in_shelf: 2, total_in_vault: 3, store_id: 1)
+Article.create(name: 'Super hiking boots', description: 'Big strong boots', price: 111.43,
+               total_in_shelf: 1, total_in_vault: 5, store_id: 3)
+Article.create(name: 'Super bowling shoes', description: 'Best antiskid shoes', price: 227.43,
+               total_in_shelf: 1, total_in_vault: 5, store_id: 3)
+
